a28444a8e6def84e6ebeecf664eed3c2671bd276
paulofumagalli <57121585+paulofumagalli@users.noreply.github.com>
2023-01-16 18:05:52 -0300

    Update covid_por_vizinhanca.sql
    EXTRACT(YEAR FROM r.date)  AS ano,
    EXTRACT(MONTH FROM r.date) AS mes,

--- dbt/first_project/models/marts/covid_por_vizinhanca.sql
+++ dbt/first_project/models/marts/covid_por_vizinhanca.sql
@@ -1,7 +1,7 @@
 SELECT
   bairro_resid_estadia                   AS bairro,
-  DATE_TRUNC(dt_notific, YEAR)           AS ano,
-  DATE_TRUNC(dt_notific, MONTH)          AS mes,
+  EXTRACT(YEAR FROM r.date)              AS ano,
+  EXTRACT(MONTH FROM r.date)             AS mes,
   COUNT(*)                               AS qtde_casos,
   SUM(IF(evolucao = "ÓBITO"     , 1, 0)) AS qtde_obitos,
   SUM(IF(evolucao = "RECUPERADO", 1, 0)) AS qtde_recuperados,
@@ -9,7 +9,7 @@ SELECT
   SUM(IF(sistema  = "SIVEP"     , 1, 0)) AS qtde_casos_graves
 FROM  {{ ref('staging_covid') }}
 WHERE dt_notific >= "2020-01-01"
-AND   bairro_resid_estadia is not null 
+AND   bairro_resid_estadia is not null
 AND   bairro_resid_estadia <> "IGNORADO"
 GROUP BY bairro, ano, mes
 ORDER BY ano, mes;
