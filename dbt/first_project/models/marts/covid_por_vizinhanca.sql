a18fe19955d89ed1d35e8146d07734c21cb5aea6
paulofumagalli <57121585+paulofumagalli@users.noreply.github.com>
2023-01-16 17:28:44 -0300

    Update covid_por_vizinhanca.sql
    ajustes na query

--- dbt/first_project/models/marts/covid_por_vizinhanca.sql
+++ dbt/first_project/models/marts/covid_por_vizinhanca.sql
@@ -1,28 +1,17 @@
-WITH covid_flags AS (
-    SELECT 
-        *,
-        CASE WHEN evolucao = "ÓBITO" THEN 1 ELSE 0 END AS FLG_OBITO,
-        CASE WHEN evolucao = "RECUPERADO" THEN 1 ELSE 0 END AS FLG_RECUPERADO,
-        CASE WHEN evolucao = "ATIVO" THEN 1 ELSE 0 END AS FLG_ATIVO,
-        CASE WHEN sistema = "SIVEP" THEN 1 ELSE 0 END AS FLG_CASO_GRAVE,
-    FROM
-        {{ ref('staging_covid') }}
-)
+-- vw_covid_por_vizinhanca
+-- create or replace view `onyx-course-371018`.`dataset`.`covid_por_vizinhanca` AS
 SELECT
-    bairro_resid_estadia AS bairro,
-    DATE_TRUNC(dt_notific, YEAR) AS ano,
-    DATE_TRUNC(dt_notific, MONTH) AS mes,
-    COUNT(*) as qtde_casos,
-    SUM(FLG_OBITO) as qtde_obitos,
-    SUM(FLG_RECUPERADO) as qtde_recuperados,
-    SUM(FLG_ATIVO) as qtde_ativos,
-    SUM(FLG_CASO_GRAVE) as qtde_casos_graves
-FROM
-    covid_flags
-GROUP BY
-    1,
-    2,
-    3
-ORDER BY
-    2,
-    3
+  bairro_resid_estadia                   AS bairro,
+  DATE_TRUNC(dt_notific, YEAR)           AS ano,
+  DATE_TRUNC(dt_notific, MONTH)          AS mes,
+  COUNT(*)                               AS qtde_casos,
+  SUM(IF(evolucao = "ÓBITO"     , 1, 0)) AS qtde_obitos,
+  SUM(IF(evolucao = "RECUPERADO", 1, 0)) AS qtde_recuperados,
+  SUM(IF(evolucao = "ATIVO"     , 1, 0)) AS qtde_ativos,
+  SUM(IF(sistema  = "SIVEP"     , 1, 0)) AS qtde_casos_graves
+FROM  {{ ref('staging_covid') }}
+WHERE dt_notific >= "2020-01-01"
+AND   bairro_resid_estadia is not null 
+AND   bairro_resid_estadia <> "IGNORADO"
+GROUP BY bairro, ano, mes
+ORDER BY ano, mes;
