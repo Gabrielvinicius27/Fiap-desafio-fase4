93c0880f5b1d59a7e70f004ae5a5552b28882c51
paulofumagalli <57121585+paulofumagalli@users.noreply.github.com>
2023-01-17 11:22:57 -0300

    Update staging_covid.sql
    de: where upper(classificacao_final) = "CONFIRMADO"
    para: where classificacao_final = "CONFIRMADO"

--- dbt/first_project/models/staging/staging_covid.sql
+++ dbt/first_project/models/staging/staging_covid.sql
@@ -1,24 +1,22 @@
 {{ config(materialized='table') }}
-
 with source_data as (
 
     select distinct 
-    UPPER(sexo) AS sexo,
-    UPPER(sistema) AS sistema,
-    UPPER(evolucao) AS evolucao,
-    REGEXP_REPLACE(NORMALIZE(UPPER(raca_cor), NFD), r'\pM', '') AS raca_cor,
-    SAFE.PARSE_DATE('%m/%d/%Y', dt_notific) AS dt_notific,
-    SAFE.PARSE_DATE('%m/%d/%Y', dt_evolucao) AS dt_evolucao,
-    UPPER(faixa_etaria) AS faixa_etaria,
-    SAFE.PARSE_DATE('%m/%d/%Y', Data_atualizacao) AS dt_atualizacao,
-    SAFE.PARSE_DATE('%m/%d/%Y', dt_inicio_sintomas) AS dt_inicio_sintomas,
-    REGEXP_REPLACE(NORMALIZE(UPPER(bairro_resid_estadia), NFD), r'\pM', '') AS bairro_resid_estadia,
-    ap_residencia_estadia,
-    UPPER(classificacao_final) AS classificacao_final
-    from {{ source('bigquery', 'covid_rj') }}
+        UPPER(sexo) AS sexo,
+        UPPER(sistema) AS sistema,
+        UPPER(evolucao) AS evolucao,
+        REGEXP_REPLACE(NORMALIZE(UPPER(raca_cor), NFD), r'\pM', '') AS raca_cor,
+        SAFE.PARSE_DATE('%m/%d/%Y', dt_notific) AS dt_notific,
+        SAFE.PARSE_DATE('%m/%d/%Y', dt_evolucao) AS dt_evolucao,
+        UPPER(faixa_etaria) AS faixa_etaria,
+        SAFE.PARSE_DATE('%m/%d/%Y', Data_atualizacao) AS dt_atualizacao,
+        SAFE.PARSE_DATE('%m/%d/%Y', dt_inicio_sintomas) AS dt_inicio_sintomas,
+        REGEXP_REPLACE(NORMALIZE(UPPER(bairro_resid_estadia), NFD), r'\pM', '') AS bairro_resid_estadia,
+        ap_residencia_estadia,
+        UPPER(classificacao_final) AS classificacao_final
+    from 
+        {{ source('bigquery', 'covid_rj') }}
 )
-
 select *
 from source_data
-where upper(classificacao_final) = "CONFIRMADO"
-
+where classificacao_final = "CONFIRMADO"
